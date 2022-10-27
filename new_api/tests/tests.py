6601864487096fada75a5be0a19d2a879b66ac3d
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-10-27 14:28:02 +0000

    updated tests

--- new_api/tests/tests.py
+++ new_api/tests/tests.py
@@ -2,7 +2,8 @@ from test_utils import *
 
 question = "what is the average salary?"
 
-#print(execute_query_csv(question, "salaries.csv"))
-
 def test_execute_query_csv():
     assert str(execute_query_csv(question, "salaries.csv")) == "{'query': 'what is the average salary?', 'result': ['80000', 'AVERAGE']}"
+    
+def test_execute_query_excel():
+    pass
