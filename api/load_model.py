e75eff2732d87782b4b49f09dfd6371a4db1e834
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-10-28 11:04:12 +0000

    docker compose not working, need to fix

--- api/load_model.py
+++ api/load_model.py
@@ -1,5 +1,6 @@
 from transformers import TapasTokenizer, TFTapasForQuestionAnswering
 
+
 model_name = "google/tapas-base-finetuned-wtq"
 model = TFTapasForQuestionAnswering.from_pretrained(model_name)
 tokenizer = TapasTokenizer.from_pretrained(model_name)
